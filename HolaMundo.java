343fc8837df0b2108d61217e2ef051bc9e53a724
Rafael Junior Gutierrez Bernal <rafas8921@gmail.com>
2024-10-24 13:30:18 -0500

    Saludo Personal

--- HolaMundo.java
+++
@@ -1,9 +0,0 @@
-/**
- * HolaMundo
- */
-public class HolaMundo {
-
-    public static void main(String[] args) {
-        System.out.println("Hola junior");
-    }
-}
---
+++ SaludoPersonal.java
@@ -0,0 +1,9 @@
+/**
+ * HolaMundo
+ */
+public class SaludoPersonal {
+
+    public static void main(String[] args) {
+        System.out.println("cesar,bienvenido al curso de programacion en java");
+    }
+}
